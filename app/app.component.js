eb5be7e715653292fcf5ff3293853c2b370c7fac
keitheous <keithchongwy@gmail.com>
2016-09-22 23:20:23 +1000

    css and media query

--- app/app.component.js
+++ app/app.component.js
@@ -16,7 +16,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <img class=\"banner\" src=\"http://placehold.it/850x150\">\n    <h1>{{title}}</h1>\n    <nav>\n      <a routerLink=\"/dashboard\" routerLinkActive=\"active\">Products</a>\n      <a routerLink=\"/about\" routerLinkActive=\"active\">About</a>\n      <a routerLink=\"/contact\" routerLinkActive=\"active\">Contact</a>\n      <a routerLink=\"/checkout\" routerLinkActive=\"active\">Checkout</a>\n      <a routerLink=\"/faqs\" routerLinkActive=\"active\">FAQs</a>\n      <a routerLink=\"/custom\" routerLinkActive=\"active\">Custom</a>\n    </nav>\n    <router-outlet></router-outlet>\n    <my-footer></my-footer>\n  ",
+            template: "\n    <img class=\"banner\" src=\"http://placehold.it/850x300\">\n    <h1 class=\"front-component\">{{title}}</h1>\n    <nav>\n      <a routerLink=\"/dashboard\" routerLinkActive=\"active\">Products</a>\n      <a routerLink=\"/about\" routerLinkActive=\"active\">About</a>\n      <a routerLink=\"/contact\" routerLinkActive=\"active\">Contact</a>\n      <a routerLink=\"/checkout\" routerLinkActive=\"active\">Checkout</a>\n      <a routerLink=\"/faqs\" routerLinkActive=\"active\">FAQs</a>\n      <a routerLink=\"/custom\" routerLinkActive=\"active\">Custom</a>\n    </nav>\n    <router-outlet class=\"front-component\"></router-outlet>\n    <my-footer></my-footer>\n  ",
             styleUrls: ['app/app.component.css']
         }), 
         __metadata('design:paramtypes', [])
