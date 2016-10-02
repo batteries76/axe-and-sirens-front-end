f6ad7cb7ec8e9011f4866ab8347534746e015e72
keitheous <keithchongwy@gmail.com>
2016-10-02 20:49:54 +1100

    styling

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,18 +5,21 @@ import { Component } from '@angular/core';
   template: `
 
     <h1>{{title}}</h1>
-    <nav>
+    <nav class="hide-on-small-only">
       <div class="nav-wrapper blue-grey lighten-1">
-        <ul>
-          <li><a routerLink="/dashboard" routerLinkActive="active">Products</a></li>
-          <li><a routerLink="/about" routerLinkActive="active">About</a></li>
-          <li><a routerLink="/contact" routerLinkActive="active">Contact</a></li>
-          <li><a routerLink="/checkout" routerLinkActive="active">Checkout</a></li>
-          <li><a routerLink="/faqs" routerLinkActive="active">FAQs</a></li>
-          <li><a routerLink="/custom" routerLinkActive="active">Custom</a></li>
-        </ul>
+        <div class="container">
+          <ul>
+            <li><a routerLink="/dashboard" routerLinkActive="active">Products</a></li>
+            <li><a routerLink="/about" routerLinkActive="active">About</a></li>
+            <li><a routerLink="/contact" routerLinkActive="active">Contact</a></li>
+            <li><a routerLink="/checkout" routerLinkActive="active">Checkout</a></li>
+            <li><a routerLink="/faqs" routerLinkActive="active">FAQs</a></li>
+            <li><a routerLink="/custom" routerLinkActive="active">Custom</a></li>
+          </ul>
+        </div>
       </div>
     </nav>
+
     <router-outlet></router-outlet>
     <my-footer></my-footer>
   `,
